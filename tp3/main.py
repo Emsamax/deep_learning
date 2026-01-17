feccd1430cafd76f984bca74cfcf8b0ab5c7960f
emsamax <martin.neron@laposte.net>
2026-01-17 23:00:46 +0100

    finish tp3

--- tp3/main.py
+++ tp3/main.py
@@ -5,8 +5,39 @@ from rnn import RNNModel
 from vocabulary import NLPDataset, Vocab,  pad_collate_fn, load_embeddings
 from torch.utils.data import DataLoader
 
+
+def train_with_special_config(config, train_loader, valid_loader, test_loader, embedding_layer, device):
+    """train on a given configuration and return the accuracy"""
+    print(f"\n>>> Testing Config: {config}")
+    
+    if config['model_type'] == 'baseline':
+        model = BaselineModel(embedding_layer, hidden_dim=config['hidden_dim']).to(device)
+    else:
+        model = RNNModel(
+            embedding_layer, 
+            rnn_type=config['rnn_type'],
+            hidden_dim=config['hidden_dim'],
+            num_layers=config['num_layers'],
+            bidirectional=config['bidirectional'],
+            dropout=config['dropout']
+        ).to(device)
+    
+    optimizer = torch.optim.Adam(model.parameters(), lr=config['lr'])
+    criterion = torch.nn.BCEWithLogitsLoss()
+    
+    history = train_model(
+        model, train_loader, valid_loader, optimizer, criterion, 
+        num_epochs=config['epochs'], device=device, clip_value=config['clip']
+    )
+    
+    final_val_acc = history['valid_accuracy'][-1]
+    return final_val_acc
+
 if __name__ == "__main__":
     #EX1
+    print("="*40)
+    print("EX 1")
+    print("="*40)
     train_dataset = NLPDataset('tp3/data/sst_train_raw.csv')
     text_vocab, label_vocab = train_dataset.build_vocab(max_size=-1, min_freq=1)
     print(f"size of vocab text: {len(text_vocab)}")
@@ -37,6 +68,9 @@ if __name__ == "__main__":
     
        
     #EX 2
+    print("="*40)
+    print("EX 2")
+    print("="*40)
     seed = 7052020
     batch_size = 10
     lr = 1e-4
@@ -90,29 +124,61 @@ if __name__ == "__main__":
     print(f"Test F1 = {test_metrics['f1']:.3f}")
     print(f"\nConfusion Matrix:\n{test_metrics['confusion_matrix']}")
     
-    #EX 3
-    rnn_type = 'LSTM' # RNN or GRU or LSTM
-    hidden_dim = 150
-    lr = 1e-4
-    clip_value = 0.25
+    #EX 3/4
+    print("="*40)
+    print("EX 3 and 4 (hyperparameters exploration)")
+    print("="*40)
+    
+    #save the hyperparameters  of the config with the best accuracy
+    best_config = {
+        'accuracy': -1.0,
+        'params': None
+    }
+    
+    rnn_types = ['RNN', 'GRU', 'LSTM']
+    hidden_sizes = [150, 300, 500]
+    num_layers_list = [1, 2, 3]
+    bidirectionals = [True, False]
+    
+    results = []
 
-    model = RNNModel(
-        embedding_layer, 
-        rnn_type=rnn_type,
-        hidden_dim=hidden_dim,
-        num_layers=2,
-        bidirectional=False
-    ).to(device)
-    
-    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-    criterion = torch.nn.BCEWithLogitsLoss()
-    
+    print("\nSTARTING HYPERPARAMETER EXPLORATION FOR OPTIMIZATION")
+    number = 1
+    for r_type in rnn_types:
+        for h_size in hidden_sizes:
+            current_config = {
+                'model_type': 'rnn',
+                'rnn_type': r_type,
+                'hidden_dim': h_size,
+                'num_layers': 2,
+                'bidirectional': True,
+                'dropout': 0.3,
+                'lr': 1e-4,
+                'epochs': 5,
+                'clip': 0.25
+            }
+      
+            print(f"CONFIG NUMBER: {number:.0f}")
+            number += 1
+            current_acc = train_with_special_config(current_config, train_loader, valid_loader, test_loader, embedding_layer, device)
+            
+            # update the best model
+            if current_acc > best_config['accuracy']:
+                print(f" Better config found with accuracy of : {current_acc:.2f}% (Old: {best_config['accuracy']:.2f}%)")
+                best_config['accuracy'] = current_acc
+                best_config['params'] = current_config
 
-    print(f"\nTRAINING {rnn_type} MODEL")
-    history = train_model(
-        model, train_loader, valid_loader, optimizer, criterion, 
-        num_epochs=10, device=device, clip_value=clip_value
-    )
+    print("\nRESULTS")
+    for res in results:
+        print(f"Type: {res[0]}, Hidden: {res[1]}, Accuracy: {res[2]:.2f}%")
+
+    seed_results = []
+    final_params = best_config['params']
+    print(f"\n=== TESTING BEST CONFIG WITH 5 SEEDS ")
+    for s in [42, 100, 500, 1000, 10000]:
+        torch.manual_seed(s)
+        np.random.seed(s)
+        acc = train_with_special_config(final_params, train_loader, valid_loader, test_loader, embedding_layer, device)
+        seed_results.append(acc)
     
-    test_metrics = evaluate(model, test_loader, criterion, device)
-    print(f"\nACCURACY ON TEST SET: {test_metrics['accuracy']:.2f}%")
+    print(f"\nFinal Statistics for Best Model: Mean = {np.mean(seed_results):.2f}%, Std = {np.std(seed_results):.2f}%")
